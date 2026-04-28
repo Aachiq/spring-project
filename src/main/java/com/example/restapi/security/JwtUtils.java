331e28e2d5d0184d5f96b2c05ed7fab215527f7f
Aachiq <hocine.aachiq@gmail.com>
2026-04-28 10:45:56 +0100

    use secret inside properties file and call it with @value("${name}")

--- src/main/java/com/example/restapi/security/JwtUtils.java
+++ src/main/java/com/example/restapi/security/JwtUtils.java
@@ -2,6 +2,7 @@ package com.example.restapi.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -9,7 +10,9 @@ import java.util.Date;
 @Component
 public class JwtUtils {
 
-    private final String JWT_SECRET = "mySuperSecretKeyThatIsVeryLongAndSecure123456789";
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
